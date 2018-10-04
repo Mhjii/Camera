71f4ecba1914919481dfb9ba08613d7fcc81e43c
Mhjii <michael.h.johnson@ttu.edu>
2018-10-04 18:55:26 -0500

    shrunk gaussian blur filter to allow detection at further distances and updated delta values to shrink range of detection

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ colorMax = (0,0,0)
 
 DeltaH = 10
 
-DeltaS = 100
-DeltaB = 100
+DeltaS = 50
+DeltaB = 75
 DeltaR = 20
 DeltaG = 20
 
 lastImg = None
 paused = False
 
-kernel = np.ones((25,25),np.float32)/625
+kernel = np.ones((21,21),np.float32)/441
 
 
 laplacian = np.array((
@@ -134,81 +134,80 @@ def show_webcam(mirror=False):
             # do so now
             if mirror:
                 img = cv2.flip(img, 1)
+
+            blur = cv2.filter2D(img, -1, kernel)
+            hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HLS_FULL)
+            mask = cv2.inRange(hsv, colorMin, colorMax)
+            # generata a result
+
+            res = cv2.bitwise_and(img, img, mask=mask)
+            # show the mask
+            cv2.imshow('mask', mask)
+            cv2.imshow('Bitwise', res)
+            # convoleOutput = convolve(mask, laplacian)
+            laplac = cv2.filter2D(mask, -1, laplacian)
+            # topRight = cv2.filter2D(mask, -1, topR)
+            # bottomLeft = cv2.filter2D(mask, -1, botL)
+            # bottomRight = cv2.filter2D(mask, -1, botR)
+
+            cv2.imshow('Conv: Edges', laplac)
+            ret, thresh = cv2.threshold(mask, 200, 255, 3)
+            im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            try:
+                maxArea = 0
+                # for cont in contours:
+                #     if cv2.contourArea(cont) > maxArea:
+                #         cnt = cont
+                cnt = contours[0]
+                rect = cv2.minAreaRect(cnt)
+                box = cv2.boxPoints(rect)
+                box = np.int0(box)
+                cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
+                cv2.drawContours(img, contours, 0, (0, 255, 255), 2)
+                points = np.array(box)
+                center = np.sum(points, 0) / 4
+                print(center[0])
+
+                cv2.drawMarker(img, (box[0][0], box[0][1]), (255, 0, 0), cv2.MARKER_CROSS)
+                cv2.drawMarker(img, (box[1][0], box[1][1]), (255, 0, 0), cv2.MARKER_CROSS)
+                cv2.drawMarker(img, (box[2][0], box[2][1]), (255, 0, 0), cv2.MARKER_CROSS)
+                cv2.drawMarker(img, (box[3][0], box[3][1]), (255, 0, 0), cv2.MARKER_CROSS)
+                cv2.drawMarker(img, (int(center[0]), int(center[1])), (255, 165, 0), cv2.MARKER_TRIANGLE_UP)
+
+            except:
+                cv2.drawMarker(img, (0, 0), (255, 165, 0), cv2.MARKER_TRIANGLE_UP)
+
+            # leftmost = tuple(laplac[laplac[:, :, 0].argmin()][0])
+            # rightmost = tuple(laplac[laplac[:, :, 0].argmax()][0])
+            # topmost = tuple(laplac[laplac[:, :, 1].argmin()][0])
+            # bottommost = tuple(laplac[laplac[:, :, 1].argmax()][0])
+
+            ## commented out to disable blob detection
+            # keypoints = detector.detect(img)
+            # blobImg = cv2.drawKeypoints(img,keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+            # img = cv2.circle(img, leftmost, 5, (0,0,255), -1)
+            # img = cv2.circle(img, rightmost, 5, (0, 0, 255), -1)
+            # img = cv2.circle(img, topmost, 5, (0, 0, 255), -1)
+            # img = cv2.circle(img, bottommost, 5, (0, 0, 255), -1)
+
+            # cont = cv2.findContours(res,cv2.RETR_FLOODFILL,cv2.CHAIN_APPROX_SIMPLE)
+
+            # contours = cv2.drawContours(img,[cont],-1,(0,255,0),2)
+            # show the original image
+            cv2.imshow('image', img)
+            # setup the callbac to allow the clicks to work
+            cv2.setMouseCallback("image", onclick, hsv)
         # if were paused keep displaying the last image
         else:
             img = lastImg
             cv2.imshow('image', img)
             cv2.setMouseCallback("image", onclick)
-            continue
         # convert the image to HSV encoding
         # this allows similar colors to be more easily selected
 
         # generate a binary mask between our min and max colors
-        blur = cv2.filter2D(img, -1, kernel)
-        hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HLS_FULL)
-        mask = cv2.inRange(hsv,colorMin,colorMax)
-        # generata a result
 
-        res = cv2.bitwise_and(img,img,mask= mask)
-        # show the mask
-        cv2.imshow('mask', mask)
-        cv2.imshow('Bitwise', res)
-        #convoleOutput = convolve(mask, laplacian)
-        laplac = cv2.filter2D(mask, -1, laplacian)
-        # topRight = cv2.filter2D(mask, -1, topR)
-        # bottomLeft = cv2.filter2D(mask, -1, botL)
-        # bottomRight = cv2.filter2D(mask, -1, botR)
-
-        cv2.imshow('Conv: Edges',laplac)
-        ret, thresh = cv2.threshold(mask, 200, 255, 3)
-        im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        try:
-            maxArea = 0
-            # for cont in contours:
-            #     if cv2.contourArea(cont) > maxArea:
-            #         cnt = cont
-            cnt = contours[0]
-            rect = cv2.minAreaRect(cnt)
-            box = cv2.boxPoints(rect)
-            box = np.int0(box)
-            cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
-            cv2.drawContours(img, contours, 0, (0, 255, 255), 2)
-            points = np.array(box)
-            center = np.sum(points,0)/4
-            print(center[0])
-
-            cv2.drawMarker(img, (box[0][0], box[0][1]), (255, 0, 0), cv2.MARKER_CROSS)
-            cv2.drawMarker(img, (box[1][0], box[1][1]), (255, 0, 0), cv2.MARKER_CROSS)
-            cv2.drawMarker(img, (box[2][0], box[2][1]), (255, 0, 0), cv2.MARKER_CROSS)
-            cv2.drawMarker(img, (box[3][0], box[3][1]), (255, 0, 0), cv2.MARKER_CROSS)
-            cv2.drawMarker(img, (int(center[0]),int(center[1])), (255, 165, 0), cv2.MARKER_TRIANGLE_UP)
-
-        except:
-            continue
-
-
-
-        #leftmost = tuple(laplac[laplac[:, :, 0].argmin()][0])
-        #rightmost = tuple(laplac[laplac[:, :, 0].argmax()][0])
-        #topmost = tuple(laplac[laplac[:, :, 1].argmin()][0])
-        #bottommost = tuple(laplac[laplac[:, :, 1].argmax()][0])
-
-        ## commented out to disable blob detection
-        #keypoints = detector.detect(img)
-        #blobImg = cv2.drawKeypoints(img,keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-        #img = cv2.circle(img, leftmost, 5, (0,0,255), -1)
-        #img = cv2.circle(img, rightmost, 5, (0, 0, 255), -1)
-        #img = cv2.circle(img, topmost, 5, (0, 0, 255), -1)
-        #img = cv2.circle(img, bottommost, 5, (0, 0, 255), -1)
-
-        #cont = cv2.findContours(res,cv2.RETR_FLOODFILL,cv2.CHAIN_APPROX_SIMPLE)
-
-        #contours = cv2.drawContours(img,[cont],-1,(0,255,0),2)
-        # show the original image
-        cv2.imshow('image', img)
-        # setup the callbac to allow the clicks to work
-        cv2.setMouseCallback("image", onclick, hsv)
         if cv2.waitKey(1) == 27:
             break  # esc to quit
         # set the last image to the current image
