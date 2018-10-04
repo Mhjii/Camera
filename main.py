3ed09dc9c9f99f1c32645140fd100719c1e743e6
Mhjii <michael.h.johnson@ttu.edu>
2018-10-03 20:04:16 -0500

    changed to contour detection; extracting bounding box and centroid

--- main.py
+++ main.py
@@ -1,24 +1,47 @@
 import cv2
 import numpy as np
+import pylivestream as stream
 
 colorMin = (0,0,0)
 colorMax = (0,0,0)
 
 DeltaH = 10
 
-DeltaS = 60
-DeltaB = 60
-
+DeltaS = 100
+DeltaB = 100
 DeltaR = 20
 DeltaG = 20
 
 lastImg = None
 paused = False
 
+kernel = np.ones((25,25),np.float32)/625
+
+
 laplacian = np.array((
 	[0, 1, 0],
 	[1, -4, 1],
 	[0, 1, 0]), dtype="int")
+
+topR = np.array((
+	[-4, 1, 1],
+	[1, 0, 0],
+	[1, 0, 0]), dtype="int")
+
+topL = np.array((
+	[1, 1, -4],
+	[0, 0, 1],
+	[0, 0, 1]), dtype="int")
+
+botR = np.array((
+	[1, 0, 0],
+	[1, 0, 0],
+	[-4, 1, 1]), dtype="int")
+
+botL = np.array((
+	[0, 0, 1],
+	[0, 0, 1],
+	[1, 1, -4]), dtype="int")
 
 # Setup SimpleBlobDetector parameters.
 params = cv2.SimpleBlobDetector_Params()
@@ -39,7 +62,7 @@ params.filterByCircularity = False
 params.minCircularity = 0.1
 
 # Filter by Convexity
-params.filterByConvexity = False
+params.filterByConvexity = True
 params.minConvexity = 0.87
 
 # Filter by Inertia
@@ -70,7 +93,7 @@ def convolve(image, kernel):
                                cv2.BORDER_REPLICATE)
     output = np.zeros((iH, iW), dtype="float32")
 
-def onclick(event, x, y,flags,frame):
+def onclick(event, x, y,flags,frame = None):
     # importing globals
     global colorMax, colorMin,lastImg,paused
     #Left mouse click selects color to mask with
@@ -89,9 +112,12 @@ def onclick(event, x, y,flags,frame):
     if event == cv2.EVENT_RBUTTONDOWN:
         lastImg = frame
         paused = not paused
+        if paused:
+            print('paused')
+        else:
+            print('unpaused')
 
-
-cv2.namedWindow("image")
+    cv2.namedWindow("image")
 
 ## this is the look that allows the webcam to playback video
 def show_webcam(mirror=False):
@@ -110,26 +136,71 @@ def show_webcam(mirror=False):
                 img = cv2.flip(img, 1)
         # if were paused keep displaying the last image
         else:
-
             img = lastImg
+            cv2.imshow('image', img)
+            cv2.setMouseCallback("image", onclick)
+            continue
         # convert the image to HSV encoding
         # this allows similar colors to be more easily selected
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HLS_FULL)
+
         # generate a binary mask between our min and max colors
+        blur = cv2.filter2D(img, -1, kernel)
+        hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HLS_FULL)
         mask = cv2.inRange(hsv,colorMin,colorMax)
         # generata a result
+
         res = cv2.bitwise_and(img,img,mask= mask)
         # show the mask
         cv2.imshow('mask', mask)
+        cv2.imshow('Bitwise', res)
+        #convoleOutput = convolve(mask, laplacian)
+        laplac = cv2.filter2D(mask, -1, laplacian)
+        # topRight = cv2.filter2D(mask, -1, topR)
+        # bottomLeft = cv2.filter2D(mask, -1, botL)
+        # bottomRight = cv2.filter2D(mask, -1, botR)
 
-        convoleOutput = convolve(mask, laplacian)
-        cvOutput = cv2.filter2D(mask, -1, laplacian)
+        cv2.imshow('Conv: Edges',laplac)
+        ret, thresh = cv2.threshold(mask, 200, 255, 3)
+        im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        try:
+            maxArea = 0
+            # for cont in contours:
+            #     if cv2.contourArea(cont) > maxArea:
+            #         cnt = cont
+            cnt = contours[0]
+            rect = cv2.minAreaRect(cnt)
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
+            cv2.drawContours(img, contours, 0, (0, 255, 255), 2)
+            points = np.array(box)
+            center = np.sum(points,0)/4
+            print(center[0])
 
-        cv2.imshow('Conv: Edges',cvOutput)
+            cv2.drawMarker(img, (box[0][0], box[0][1]), (255, 0, 0), cv2.MARKER_CROSS)
+            cv2.drawMarker(img, (box[1][0], box[1][1]), (255, 0, 0), cv2.MARKER_CROSS)
+            cv2.drawMarker(img, (box[2][0], box[2][1]), (255, 0, 0), cv2.MARKER_CROSS)
+            cv2.drawMarker(img, (box[3][0], box[3][1]), (255, 0, 0), cv2.MARKER_CROSS)
+            cv2.drawMarker(img, (int(center[0]),int(center[1])), (255, 165, 0), cv2.MARKER_TRIANGLE_UP)
+
+        except:
+            continue
+
+
+
+        #leftmost = tuple(laplac[laplac[:, :, 0].argmin()][0])
+        #rightmost = tuple(laplac[laplac[:, :, 0].argmax()][0])
+        #topmost = tuple(laplac[laplac[:, :, 1].argmin()][0])
+        #bottommost = tuple(laplac[laplac[:, :, 1].argmax()][0])
 
         ## commented out to disable blob detection
-        #keypoints = detector.detect(res)
+        #keypoints = detector.detect(img)
         #blobImg = cv2.drawKeypoints(img,keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+        #img = cv2.circle(img, leftmost, 5, (0,0,255), -1)
+        #img = cv2.circle(img, rightmost, 5, (0, 0, 255), -1)
+        #img = cv2.circle(img, topmost, 5, (0, 0, 255), -1)
+        #img = cv2.circle(img, bottommost, 5, (0, 0, 255), -1)
 
         #cont = cv2.findContours(res,cv2.RETR_FLOODFILL,cv2.CHAIN_APPROX_SIMPLE)
 
@@ -147,6 +218,7 @@ def show_webcam(mirror=False):
 
 # call the main loop
 def main():
+    stream.Webcam
     show_webcam(mirror=True)
 
 
